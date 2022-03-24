e4e0135042266d689832a90df8ef5918ea6d2843
Chip Hill <chiphill@chips-mbp.lan>
2022-03-24 04:42:34 -0400

    working on it, here is debug code

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-//Your Code here
+//add user question
+// window.prompt('Do you head left or right?')
+//get the answer
+//let answer = window.prompt('Do you head left or right?')
+// console.log(answer)
+// let firstAnswer  = window.prompt('Do you head left or right?')
+// console.log(firstAnswer === 'left')
+let firstAnswer  = window.prompt('Do you head left or right?')
+window.alert(firstAnswer)
+
+if (firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. It scampers off down a small hole, just large enough for you to crawl through. Do you follow it, or continue on your path?`)
+    window.alert(secondAnswer) 
+    }
+    if (secondAnswer === 'follow'){
+        let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.")
+        window.alert(thirdAnswer)
+    }
+ 
+// }
+
+
+// let firstAnswer  = window.prompt('Do you head left or right?')
+// if(firstAnswer === 'left'){
+//     let secondAnswer = window.prompt(`You come across a stray cat. 
+// It scampers off down a small hole, just large enough for you to crawl through. 
+// Do you follow it, or continue on your path?`)
+
+// } else if(firstAnswer === 'right') {
+//     let secondAnswer = window.prompt(`You come across a snoring dragon. 
+// On the other side of him, you see a shiny chest of treasure. Another path would 
+// lead you away from the dragon altogether. Which path do you take?`)
+
+
+// if(secondAnswer == 'follow') {let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')}
+// if(thirdAnswer != 'stay'){let catContinue = window.prompt("You won't get back to the cats so you might as well continue")}
+//  else {window.alert("Guess your traveling days are done.  Here is some antihistimine, enjoy your life")}
+
+ // if(secondAnswer = "follow"){
+//     let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subisting off the inexplicably warm soup.  They are content with you staying, but you wondier if you should alert the world to this magical safe haven.")
+ //if(thirdAnswer === "stay")
+    // window.prompt("After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.")
+//else window.prompt("You live happily amongst the cats for the rest of your days.")
+
+
+
+
+
+
+// } else if(firstAnswer === 'right') {
+//     let secondAnswer = window.prompt(`You come across a snoring dragon. 
+// On the other side of him, you see a shiny chest of treasure. Another path would 
+// lead you away from the dragon altogether. Which path do you take?`)
+
+// }
